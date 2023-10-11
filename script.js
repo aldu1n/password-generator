09e997749f2afe87038a945749f8882286d07f52
Bogdan Tkachuk <cnndo14@gmail.com>
2023-10-11 15:01:59 -0400

    added confirm and if statemants for each option

--- script.js
+++ script.js
@@ -24,14 +24,50 @@ function createPassword(){
   var length = window.prompt("Enter your desired password length (between 8 and 128 characters)");
   var min = 8;
   var max = 128;
-  if(parseInt(length) && (length >= min && length <= max)){
+  if (length <= max && length >= min ){
     return length;
   }
   };
 
 
   var passwordLength = getPasswordLength();
+  if (typeof passwordLength === "undefined"){
+    alert("Incorrect input! Please enter a number between 8 and 128.");
+    return createPassword();
+  }
   console.log(passwordLength);
+
+  var confirmUpperCase = window.confirm ("Click OK, if you want your password to include uppercase letters.")
+  if (confirmUpperCase){
+    alert ("Your password will include uppercase letters!");
+  }
+  else {
+    alert ("Your password won't include uppercase letters!");
+  }
+
+  var confirmLowerCase = window.confirm ("Click OK, if you want your password to include lowercase letters.")
+  if (confirmLowerCase){
+    alert ("Your password will include lowercase letters!");
+  }
+  else {
+    alert ("Your password won't include lowercase letters!");
+  }
+
+  var confirmNumbers = window.confirm ("Click OK, if you want your password to include numbers.")
+  if (confirmNumbers){
+    alert ("Your password will include numbers!");
+  }
+  else {
+    alert ("Your password won't include numbers!");
+  }
+
+  var confirmSymbols = window.confirm ("Click OK, if you want your password to include symbols.")
+  if (confirmSymbols){
+    alert ("Your password will include symbols!");
+  }
+  else {
+    alert ("Your password won't include symbols!");
+  }
 }
 
 
