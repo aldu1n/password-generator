009094505e074f865956efb483408963a297aad7
Bogdan Tkachuk <cnndo14@gmail.com>
2023-10-11 13:30:00 -0400

    added function for determening password length

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var characterSets = {
+  upperCase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+  numbers: '0123456789',
+  symbols: '!@#$%^&*()_+[]{}|;:,.<>?'
+};
+// create the function here //done 
 
-  passwordText.value = password;
+// prompt to get the number of characters in password // done
 
+// need 4 ifs with 4 confirmExpressionconditionals
+
+// processing...
+
+// return password
+
+//   return "turtle"; // if turtle is a password
+
+function createPassword(){
+  var getPasswordLength = function() {
+  var length = window.prompt("Enter your desired password length (between 8 and 128 characters)");
+  var min = 8;
+  var max = 128;
+  if(parseInt(length) && (length >= min && length <= max)){
+    return length;
+  }
+  };
+
+
+  var passwordLength = getPasswordLength();
+  console.log(passwordLength);
 }
 
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = createPassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.setAttribute("style", ":hover")
+generateBtn.addEventListener("click", createPassword);
